44a0c98ce9132a7bbce3430d02bb9fa987b1b6f0
acomma <acomma@yeah.net>
2022-10-22 20:02:36 +0800

    feature: ID 生成器

---
+++ src/main/java/me/acomma/duck/common/id/ID.java
@@ -0,0 +1,34 @@
+package me.acomma.duck.common.id;
+
+import java.security.SecureRandom;
+
+/**
+ * ID 生成器。
+ *
+ * <p><b>警告</b>：当前的实现有缺陷，不能在生产环境使用。
+ */
+public final class ID {
+    private static final IdWorker worker;
+
+    /*
+     * 这里模仿 MongoDB ObjectId 方式随机生成 datacenterId 和 workerId。
+     */
+    static {
+        SecureRandom secureRandom = new SecureRandom();
+        int datacenterId = secureRandom.nextInt(32);
+        int workerId = secureRandom.nextInt(32);
+
+        worker = new IdWorker(workerId, datacenterId);
+    }
+
+    private ID() {
+    }
+
+    /**
+     * 生成下一个 ID。
+     * @return 下一个 ID
+     */
+    public static long nextId() {
+        return worker.nextId();
+    }
+}
---
+++ src/main/java/me/acomma/duck/common/id/IdWorker.java
@@ -0,0 +1,73 @@
+package me.acomma.duck.common.id;
+
+/**
+ * See <a href="https://github.com/twitter-archive/snowflake/blob/snowflake-2010/src/main/scala/com/twitter/service/snowflake/IdWorker.scala">Snowflake IdWorker</a>.
+ */
+public class IdWorker {
+    private final long twepoch = 1288834974657L;
+
+    private final long workerIdBits = 5L;
+    private final long datacenterIdBits = 5L;
+    private final long maxWorkerId = -1L ^ (-1L << workerIdBits);
+    private final long maxDatacenterId = -1L ^ (-1L << datacenterIdBits);
+    private final long sequenceBits = 12L;
+
+    private final long workerIdShift = sequenceBits;
+    private final long datacenterIdShift = sequenceBits + workerIdBits;
+    private final long timestampLeftShift = sequenceBits + workerIdBits + datacenterIdBits;
+    private final long sequenceMask = -1L ^ (-1L << sequenceBits);
+
+    private long sequence = 0L;
+
+    private long lastTimestamp = -1L;
+
+    private long workerId;
+    private long datacenterId;
+
+    public IdWorker(long workerId, long datacenterId) {
+        if (workerId > maxWorkerId || workerId < 0) {
+            throw new RuntimeException(String.format("worker Id can't be greater than %d or less than 0", maxWorkerId));
+        }
+
+        if (datacenterId > maxDatacenterId || datacenterId < 0) {
+            throw new RuntimeException(String.format("datacenter Id can't be greater than %d or less than 0", maxDatacenterId));
+        }
+
+        this.workerId = workerId;
+        this.datacenterId = datacenterId;
+    }
+
+    public synchronized long nextId() {
+        long timestamp = timeGen();
+
+        if (timestamp < lastTimestamp) {
+            System.out.println(String.format("clock is moving backwards.  Rejecting requests until %d.", lastTimestamp));
+            throw new RuntimeException(String.format("Clock moved backwards.  Refusing to generate id for %d milliseconds", lastTimestamp - timestamp));
+        }
+
+        if (lastTimestamp == timestamp) {
+            sequence = (sequence + 1) & sequenceMask;
+            if (sequence == 0) {
+                timestamp = tilNextMillis(lastTimestamp);
+            }
+        } else {
+            sequence = 0;
+        }
+
+        lastTimestamp = timestamp;
+
+        return ((timestamp - twepoch) << timestampLeftShift) | (datacenterId << datacenterIdShift) | (workerId << workerIdShift) | sequence;
+    }
+
+    private long tilNextMillis(long lastTimestamp) {
+        long timestamp = timeGen();
+        while (timestamp <= lastTimestamp) {
+            timestamp = timeGen();
+        }
+        return timestamp;
+    }
+
+    private long timeGen() {
+        return System.currentTimeMillis();
+    }
+}
