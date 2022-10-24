88eb756579fcbc9ef208e5894cc37a0cce7699e8
acomma <acomma@yeah.net>
2022-10-24 22:13:40 +0800

    feature: 定义用户实体类

---
+++ src/main/java/me/acomma/duck/dal/model/user/UserEntity.java
@@ -0,0 +1,36 @@
+package me.acomma.duck.dal.model.user;
+
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import me.acomma.duck.common.model.BaseEntity;
+
+import java.io.Serial;
+
+/**
+ * 用户实体类。
+ */
+@Getter
+@Setter
+@ToString(callSuper = true)
+@NoArgsConstructor
+@SuperBuilder
+public class UserEntity extends BaseEntity {
+    @Serial
+    private static final long serialVersionUID = -419467560420367692L;
+
+    /**
+     * 用户 ID。
+     */
+    private Long userId;
+
+    /**
+     * 用户名。
+     */
+    private String username;
+
+    /**
+     * 密码。
+     */
+    @ToString.Exclude
+    private String password;
+}
