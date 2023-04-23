37dddeb4dfad94d32294e02c4805069365bad544
acomma <acomma@yeah.net>
2023-04-23 18:12:48 +0800

    refactor: 修改模块为接口，不在这里定义具体模块

--- duck-util/src/main/java/me/acomma/duck/util/Module.java
+++ duck-util/src/main/java/me/acomma/duck/util/Module.java
@@ -3,42 +3,16 @@ package me.acomma.duck.util;
 /**
  * 定义项目中有那些模块，并给每一个模块分配一个编码。
  *
- * <p>模块编码定义为整数，取值范围为 10 ~ 99。
+ * <p>模块编码定义为整数，取值范围为 10 ~ 99，作为业务错误码 {@link me.acomma.duck.util.code.BusinessErrorCode} 的前两位。
  */
-public enum Module implements Enumerable<Integer> {
-    /**
-     * 用户模块。
-     */
-    USER(10),
-
-    /**
-     * 角色模块。
-     */
-    ROLE(11),
-
-    /**
-     * 菜单模块。
-     */
-    MENU(12);
-
-    /**
-     * 模块的编码。
-     */
-    private final Integer code;
-
-    Module(Integer code) {
-        this.code = code;
-    }
-
+public interface Module extends Enumerable<Integer> {
     @Override
-    public Integer value() {
-        return code;
+    default Integer value() {
+        return code();
     }
 
     /**
      * 返回模块的编码。
      */
-    public Integer code() {
-        return code;
-    }
+    Integer code();
 }
