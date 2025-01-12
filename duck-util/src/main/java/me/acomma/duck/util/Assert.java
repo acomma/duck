02a993c2abb044fc3c8a0b07100616e3ef8fe887
acomma <acomma@yeah.net>
2025-01-12 14:57:56 +0800

    feature: 业务断言工具类

---
+++ duck-util/src/main/java/me/acomma/duck/util/Assert.java
@@ -0,0 +1,129 @@
+package me.acomma.duck.util;
+
+import me.acomma.duck.util.code.BusinessErrorCode;
+import me.acomma.duck.util.exception.BusinessException;
+
+import java.util.Collection;
+import java.util.Map;
+
+public abstract class Assert {
+    public static void isTrue(boolean expression, BusinessErrorCode code) {
+        if (!expression) {
+            throw new BusinessException(code);
+        }
+    }
+
+    public static void isTrue(boolean expression, BusinessErrorCode code, String message) {
+        if (!expression) {
+            throw new BusinessException(code, message);
+        }
+    }
+
+    public static void isFalse(boolean expression, BusinessErrorCode code) {
+        if (expression) {
+            throw new BusinessException(code);
+        }
+    }
+
+    public static void isFalse(boolean expression, BusinessErrorCode code, String message) {
+        if (expression) {
+            throw new BusinessException(code, message);
+        }
+    }
+
+    public static void isNull(Object object, BusinessErrorCode code) {
+        if (object != null) {
+            throw new BusinessException(code);
+        }
+    }
+
+    public static void isNull(Object object, BusinessErrorCode code, String message) {
+        if (object != null) {
+            throw new BusinessException(code, message);
+        }
+    }
+
+    public static void notNull(Object object, BusinessErrorCode code) {
+        if (object == null) {
+            throw new BusinessException(code);
+        }
+    }
+
+    public static void notNull(Object object, BusinessErrorCode code, String message) {
+        if (object == null) {
+            throw new BusinessException(code, message);
+        }
+    }
+
+    public static void isEmpty(Object[] array, BusinessErrorCode code) {
+        if (array != null && array.length != 0) {
+            throw new BusinessException(code);
+        }
+    }
+
+    public static void isEmpty(Object[] array, BusinessErrorCode code, String message) {
+        if (array != null && array.length != 0) {
+            throw new BusinessException(code, message);
+        }
+    }
+
+    public static void notEmpty(Object[] array, BusinessErrorCode code) {
+        if (array == null || array.length == 0) {
+            throw new BusinessException(code);
+        }
+    }
+
+    public static void notEmpty(Object[] array, BusinessErrorCode code, String message) {
+        if (array == null || array.length == 0) {
+            throw new BusinessException(code, message);
+        }
+    }
+
+    public static void isEmpty(Collection<?> collection, BusinessErrorCode code) {
+        if (collection != null && !collection.isEmpty()) {
+            throw new BusinessException(code);
+        }
+    }
+
+    public static void isEmpty(Collection<?> collection, BusinessErrorCode code, String message) {
+        if (collection != null && !collection.isEmpty()) {
+            throw new BusinessException(code, message);
+        }
+    }
+
+    public static void notEmpty(Collection<?> collection, BusinessErrorCode code) {
+        if (collection == null || collection.isEmpty()) {
+            throw new BusinessException(code);
+        }
+    }
+
+    public static void notEmpty(Collection<?> collection, BusinessErrorCode code, String message) {
+        if (collection == null || collection.isEmpty()) {
+            throw new BusinessException(code, message);
+        }
+    }
+
+    public static void isEmpty(Map<?, ?> map, BusinessErrorCode code) {
+        if (map != null && !map.isEmpty()) {
+            throw new BusinessException(code);
+        }
+    }
+
+    public static void isEmpty(Map<?, ?> map, BusinessErrorCode code, String message) {
+        if (map != null && !map.isEmpty()) {
+            throw new BusinessException(code, message);
+        }
+    }
+
+    public static void notEmpty(Map<?, ?> map, BusinessErrorCode code) {
+        if (map == null || map.isEmpty()) {
+            throw new BusinessException(code);
+        }
+    }
+
+    public static void notEmpty(Map<?, ?> map, BusinessErrorCode code, String message) {
+        if (map == null || map.isEmpty()) {
+            throw new BusinessException(code, message);
+        }
+    }
+}
